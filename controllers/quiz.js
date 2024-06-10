c9ca362cfc45929f6160d56324be7befa031fbe5
Uditkishore <uditkishor4292@gmail.com>
2024-06-11 02:03:46 +0530

    modified quiz

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -20,7 +20,39 @@ const postQuiz = async (req, res) => {
     }
 };
 
+const updateQuiz = async (req, res) => {
+    const { body, params: { uuid } } = req;
+    try {
+        const savedQuiz = await Quiz.findOneAndUpdate({ _id: uuid }, body, { new: true });
+        res.status(200).json(savedQuiz);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const deleteQuiz = async (req, res) => {
+    const { uuid } = req.params;
+    try {
+        const deletedQuiz = await Quiz.findByIdAndDelete(uuid);
+        res.status(200).json(deletedQuiz);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const deleteAll = async (req, res) => {
+    try {
+        await Quiz.deleteMany({});
+        res.status(200).json({ status: true, message: "All quizzes deleted successfully." });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
 module.exports = {
     getAllQuiz,
-    postQuiz
+    postQuiz,
+    updateQuiz,
+    deleteQuiz,
+    deleteAll
 };
